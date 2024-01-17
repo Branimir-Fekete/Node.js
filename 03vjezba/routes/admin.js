757566df94baa5a7a20a87a470ae890289da6382
Branimir-Fekete <branimir.fekete@gmail.com>
2024-01-17 17:23:53 +0100

    Projekti

---
+++ 03vjezba/routes/admin.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsControler = require('../controllers/products');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', productsControler.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productsControler.postAddProduct);
+
+module.exports = router;
---
+++ 03vjezba/routes/shop.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsControler = require('../controllers/products');
+
+const router = express.Router();
+
+router.get('/', productsControler.getProducts);
+
+module.exports = router;
